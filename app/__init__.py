ff2e60b77b05e4a817f8eab994c8b37c7eeade10
Kevin Zheng <coffeevector@gmail.com>
2020-02-28 20:09:28 -0800

    database-model: Adds models.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,13 @@
 from flask import Flask
-
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 app = Flask(__name__, template_folder='templates')
 
-from app import routes, config
+from app import config
 app.config.from_object(config.Config)
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+
+from app import routes, models
