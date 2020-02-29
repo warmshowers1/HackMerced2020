7efec9c185403df32034da6ac70c3d112e3a93fc
Kevin Zheng <coffeevector@gmail.com>
2020-02-28 19:15:36 -0800

    Database setup

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,12 @@
 from flask import Flask
 
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
 app = Flask(__name__, template_folder='templates')
 
-from app import routes
+from app import routes, config
+app.config.from_object(config.Config)
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
