44b564de0b2a702c2fb8692a6f14544d79b534d5
Alex Woersching <ajw006@gmail.com>
2020-03-01 03:09:44 -0800

    Edits main.py for better musical performance
    Adds title and author, as well as better centers the music on the page.

--- app/main.py
+++ app/main.py
@@ -36,7 +36,6 @@ def freq2note(freq: int):
 if __name__ == '__main__':
     freqs = wav2freq("drumspiano.wav", 0.25)
     notes = [freq2note(f) for f in freqs]
-    #print(notes)
 
 song = []
 #notes = ["a", "b", "c", "d", "e", "f", "g", "r"]
@@ -47,20 +46,21 @@ amounts = [0 for i in range(12)]
 
 for i in notes:
     b = i % 12
-    if i >= -53 and i <= -44:
-        song.append(letters[b])# + ",,, ")
-    if i > -44 and i <= -34:
-        song.append(letters[b])# + ",, ")
-    if i > -34 and i <= -24:
-        song.append(letters[b])# + ", ")
-    if i > -24 and i <= -14:
-        song.append(letters[b])
-    if i > -14 and i <= -4:
-        song.append(letters[b])# + "\' ")
-    if i > -4 and i <= 6:
-        song.append(letters[b])# + "\'\' ")
-    if i > 6 and i <= 15:
-        song.append(letters[b])# + "\'\'\' ")
+    song.append(letters[b])
+    #if i >= -53 and i <= -44:
+    #    song.append(letters[b])# + ",,, ")
+    #if i > -44 and i <= -34:
+    #    song.append(letters[b])# + ",, ")
+    #if i > -34 and i <= -24:
+    #    song.append(letters[b])# + ", ")
+    #if i > -24 and i <= -14:
+    #    song.append(letters[b])
+    #if i > -14 and i <= -4:
+    #    song.append(letters[b])# + "\' ")
+    #if i > -4 and i <= 6:
+    #    song.append(letters[b])# + "\'\' ")
+    #if i > 6 and i <= 15:
+    #    song.append(letters[b])# + "\'\'\' ")
     amounts[b] += 1
 
 # These two indicies don't have relevant flat accidentals
@@ -75,11 +75,24 @@ for i in range((len(amounts) - 2), -1, -2):
 
 begin = """\\documentclass[a4paper]{article}
 \\usepackage{lilypond}
-\\usepackage[margin=0.5]{geometry}
+\\usepackage[margin=0.25in]{geometry}
+\\addtolength{\\textheight}{1in}
+
+\\header{
+    title = \"Drums and Piano\"
+    composer = \"Bill from Craigslist\"
+}
 
 \\begin{document}
 \\begin{lilypond}
-\\relative c'' {
+"""
+middle_start = """ \\relative c''{
+\\clef \"treble\"
+\\time 3/4
+"""
+middle = """}
+{\\clef \"treble\"
+\\time 3/4
 """
 end = """   }
 \\end{lilypond}
@@ -116,14 +129,32 @@ def key_finder(key: int):
 f.write(begin)
 key_finder(key)
 
-for i in range(3, len(song), 4):
-    f.write(song[i-3] + " " + song[i-2] + " " + song[i-1] + " " + song[i] + "\n")
-if len(song) % 4 == 3:
-    f.write(song[-3] + " " + song[-2] + " " + song[-1] + "\n")
-if len(song) % 4 == 2:
-    f.write(song[-2] + " " + song[-1] + "\n")
-if len(song) % 4 == 1:
-    f.write(song[-1] + "\n")
+f.write(middle_start)
+key_finder(key)
+arr = [2, (len(song) / 3), 2*(len(song) / 3), len(song)]
+for j in range(int(arr[0]),int(arr[1]) + 1, 3):
+    f.write(song[j-2] + " " + song[j-1] + " " + song[j] + " | ")
+if len(song) % 3 == 2:
+    f.write(song[-2] + " " + song[-1] + " | ")
+if len(song) % 3 == 1:
+    f.write(song[-1] + " | ")
+f.write(middle)
+key_finder(key)
+for j in range(int(arr[1]),int(arr[2]) + 1, 3):
+    f.write(song[j-2] + " " + song[j-1] + " " + song[j] + " | ")
+if len(song) % 3 == 2:
+    f.write(song[-2] + " " + song[-1] + " | ")
+if len(song) % 3 == 1:
+    f.write(song[-1] + " | ")
+f.write(middle)
+key_finder(key)
+for j in range(int(arr[2]), int(arr[3]), 3):
+    f.write(song[j-2] + " " + song[j-1] + " " + song[j] + " | ")
+if len(song) % 3 == 2:
+    f.write(song[-2] + " " + song[-1] + " | ")
+if len(song) % 3 == 1:
+    f.write(song[-1] + " | ")
+
 f.write(end)
 
 f.close()
